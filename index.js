04ac149ed2b0830903383a34538cc25219b6433c
avatarvb <avatarvb@gmail.com>
2018-03-16 20:54:43 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const socket = require('socket.io');
+//
+// // App setup
+// const app = express();
+// const server = app.listen(4000, () => {
+//   console.log('listening to requests on port 4000');
+// });
+//
+// // static files
+// app.use(express.static('public'));
+//
+//
+// // socket Setup
+// const io = socket(server);
+// io.on('connection', (socket) => {
+//   console.log('made socket connection', socket.id);
+// });
